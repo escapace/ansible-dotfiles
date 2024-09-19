5f14cacf89418247c461d80a946e63cde7b400ae
yyxi <178086254+yyxi@users.noreply.github.com>
2024-09-19 17:13:54 +0000

    chore: workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,17 @@
 
 import { escapace, compose } from 'eslint-config-escapace'
 
-export default compose(escapace(), {
-  rules: {
-    'yaml/sort-keys': 'off',
+export default compose(
+  escapace(),
+  {
+    rules: {
+      'yaml/sort-keys': 'off',
+    },
   },
-})
+  {
+    files: ['**/molecule.yml'],
+    rules: {
+      'yaml/file-extension': 'off',
+    },
+  },
+)
